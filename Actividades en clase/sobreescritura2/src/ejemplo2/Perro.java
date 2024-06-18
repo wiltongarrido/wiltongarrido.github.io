c91f1fc2ff2ed1c3820264b4b2235513b37ad8e0
wiltongarrido <167437701+wiltongarrido@users.noreply.github.com>
2024-06-18 02:40:58 -0500

    Add files via upload

---
+++ Actividades en clase/sobreescritura2/src/ejemplo2/Perro.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejemplo2;
+
+/**
+ *
+ * @author Constanza
+ */
+public class Perro extends Animal{
+    
+    public void comer(){
+        System.out.println("Estoy comiendo Pedigree");
+    }
+    
+}
---
+++ Actividades en clase/sobreescritura2/src/ejemplo2/Persona.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejemplo2;
+
+/**
+ *
+ * @author Constanza
+ */
+public class Persona extends Animal {
+    
+    
+    public void comer() {
+        System.out.println("Estoy comiendo bandeja paisa");
+    }
+    
+}
